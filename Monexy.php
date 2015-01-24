37d3920f1396b5f1692e6c9483d98bbeb129be2b
Roman Bahatyi <rbagatyi@gmail.com>
2015-01-24 14:27:55 +0200

    Create Monexy.php

---
+++ Monexy.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright Copyright &copy; Roman Bahatyi, richweber.net, 2015
+ * @package yii2-google-translate-api
+ * @version 1.0.0
+ */
+namespace richweber\monexy;
+/**
+ * Yii2 extension for Monexy API
+ *
+ * @link https://www.monexy.ua/ua/api
+ * @author Roman Bahatyi <rbagatyi@gmail.com>
+ * @since 1.0
+ */
+class Translate
+{
+    //
+}
